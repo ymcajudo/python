507adca0a2ce33fabd125ad83fda9796c54f4a01
ymcajudo <ymcajudo@gmail.com>
2022-11-23 18:12:51 +0900

    v1.02

--- TCPserver_2nd.py
+++ TCPserver_2nd.py
@@ -7,9 +7,6 @@ import socket
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-a = 1
-print(a)
-
 while True:
 
     socServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -37,8 +34,5 @@ while True:
     """
     if str(data,'utf-8') == "kill the process":
         break
-    #....
     """
-    
-
-    
+  
