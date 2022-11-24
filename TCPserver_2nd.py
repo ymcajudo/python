3c647a3b2d84535d11b23e126612e883a46f6915
ymcajudo <ymcajudo@gmail.com>
2022-11-24 10:50:51 +0900

    updated in 2022-11-24

--- TCPserver_2nd.py
+++ TCPserver_2nd.py
@@ -4,11 +4,20 @@
 
 import socket
 
+"""
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+"""
+
+#"""
+try:
+    HOST = input("Input the Server's IP: ")
+    PORT = int(input("Input the Server's Port: "))
+except:
+    print("Please, check the IP and Port number.")
+#"""
 
 while True:
-
     socServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socServer.bind((HOST, PORT))
     socServer.listen()
